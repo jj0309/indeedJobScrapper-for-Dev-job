29f3c358462fd57e4aceeb98b50361cb6751a05a
ka-son chau <ka.alex.chau@gmail.com>
2020-03-21 20:25:10 -0400

    added next page

--- indeedJobScrapper.py
+++ indeedJobScrapper.py
@@ -17,25 +17,35 @@ def findJobs(URL):
     joblist=[]
     index=0
     for job in jobs:
-        print(index,': ',job.findAll(text=True),end='\n')
+        print(index,': ',job.findAll(text=True),'\n')
         joblist.append('https://ca.indeed.com'+job['href'])
         index+=1
     return joblist
 
-def openJob(returnedJobList):
+def openJob(returnedJobList,URL):
     userChoice=0
+    jobListToShow = returnedJobList
+    baseURL = URL
+    pageURL = 'https://ca.indeed.com/jobs?q=Web+Developer&l=Montr%C3%A9al%2C+QC&start='
+    pageIndex = 0
     while(userChoice!='n'):
         print('\n\ninput the index to the job listing')
-        print('type n to end the search')
+        print('type n to end the search / "d" for next page')
         userChoice = input()
-        if userChoice!='n':
+        if userChoice == 'n':
+            break
+        elif userChoice == 'd':
+            pageIndex+=1
+            jobListToShow = findJobs(pageURL+str(pageIndex*10))
+        else:
             webbrowser.open(returnedJobList[int(userChoice)])
+
 
 def main():
     URL = 'https://ca.indeed.com/Web-Developer-jobs-in-Montr%C3%A9al,-QC'
     
     returnedJobList = findJobs(URL)
-    openJob(returnedJobList)
+    openJob(returnedJobList,URL)
     
     
 
